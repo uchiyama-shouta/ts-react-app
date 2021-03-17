de067ee5f43ccb9ca86da7d6a5c3e6ad4127671f
utiyama shouta <67624765+uchiyama-shouta@users.noreply.github.com>
2021-03-17 13:47:08 +0900

    Initial commit

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,26 @@
+// types
+import { VFC } from "react";
+import { UserProfile } from "../types/userProfile";
+// styles
+import { style } from "../style/style";
+
+type Props = {
+  user: UserProfile;
+};
+
+export const UserCard: VFC<Props> = (props) => {
+  const { user } = props;
+
+  return (
+    <div style={style}>
+      <dl>
+        <dt>名前</dt>
+        <dd>{user.name}</dd>
+        <dt>メール</dt>
+        <dd>{user.email}</dd>
+        <dt>住所</dt>
+        <dd>{user.address}</dd>
+      </dl>
+    </div>
+  );
+};
---
+++ src/style/style.ts
@@ -0,0 +1,6 @@
+export const style = {
+  border: "1px solid #ccc",
+  borderRadius: "8px",
+  padding: "0 16px",
+  margin: "8px"
+};
